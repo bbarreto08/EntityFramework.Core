10bd497778ec53fdba0bf6414c4b5651c7f0914c
Bruno <brunocbarreto2012@gmail.com>
2025-07-20 17:54:14 -0300

    Showing how to setup Entity Framework Core

---
+++ EntityFramework.Core/GettingStarted/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GettingStarted
+{
+    internal class AppDbContext : DbContext
+    {
+        private const string ConnectionString = @"  Server=localhost,1433;
+                                                    Database=ExampleDataBase;
+                                                    User Id=sa;
+                                                    Password=Selecttop10*from;
+                                                    Trusted_Connection=false;
+                                                    TrustServerCertificate=true";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
---
+++ EntityFramework.Core/GettingStarted/Program.cs
@@ -0,0 +1,19 @@
+﻿using GettingStarted;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new AppDbContext();
+context.Database.OpenConnection();
+
+bool readyToConnect = context.Database.CanConnect();
+
+if(readyToConnect)
+{
+    Console.WriteLine("Connection to the database was successful.");
+}
+else
+{
+    Console.WriteLine("Failed to connect to the database.");
+}
+
+
+Console.ReadKey();
