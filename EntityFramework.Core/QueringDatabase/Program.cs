c595665aef41125520f9ad7570474892a373ba10
Bruno <brunocbarreto2012@gmail.com>
2025-07-27 20:24:04 -0300

    Simple EF Core examples doing , Query, Insert and Update.

--- EntityFramework.Core/QueringDatabase/Program.cs
+++ EntityFramework.Core/QueringDatabase/Program.cs
@@ -1,12 +1,49 @@
 ﻿using QueringDatabase;
+using QueringDatabase.Models;
 
 var appDbContext = new AppDbContext();
 
-var clients = appDbContext.Client.ToList();
+Console.WriteLine("Starting query");
+QueryClients();
+Console.WriteLine("Query ended!");
 
-foreach (var client in clients)
+Console.WriteLine("Starting insert");
+InsertClient();
+Console.WriteLine("Insert ended!");
+
+Console.WriteLine("Starting update");
+UpdateClient();
+Console.WriteLine("Update ended!");
+
+
+void QueryClients()
 {
-    Console.WriteLine($"Id: {client.ClientId}, Nome: {client.Name}");
+    var clients = appDbContext.Client.ToList();
+
+    foreach (var client in clients)
+    {
+        Console.WriteLine($"Id: {client.ClientId}, Nome: {client.Name}");
+    }
 }
 
-Console.ReadLine();
+void InsertClient()
+{
+    var client = new Client
+    {
+        Name = "New Client"
+    };
+
+    appDbContext.Client.Add(client);
+    appDbContext.SaveChanges();
+}
+
+void UpdateClient()
+{
+    var client = appDbContext.Client.FirstOrDefault(c => c.Name == "New Client");
+    
+    if (client != null)
+    {
+        client.Name = "Name Updated";
+        appDbContext.SaveChanges();
+    }
+}
